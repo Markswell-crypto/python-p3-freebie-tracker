6c9b49aa28f56a3eb145a5b466df86f11967219e
Markswell <osoromarkswell80@gmail.com>
2023-12-12 21:14:20 +0300

    Assignment Complete

---
+++ lib/migrations/versions/828f71db7fa4_create_models.py
@@ -0,0 +1,24 @@
+"""create models
+
+Revision ID: 828f71db7fa4
+Revises: 5f72c58bf48c
+Create Date: 2023-12-12 21:12:28.482937
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '828f71db7fa4'
+down_revision = '5f72c58bf48c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,33 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Freebie, Company, Dev, Base
+
+engine = create_engine('sqlite:///freebies.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base.metadata.create_all(engine)
+
+dev1 = Dev(name='Shekhs')
+dev2 = Dev(name='Ezii')
+dev3 = Dev(name='kaka')
+
+company1 = Company(name='Company A', founding_year=2001)
+company2 = Company(name='Company B', founding_year=2020)
+company3 = Company(name='Company C', founding_year=2021)
+
+freebie1 = Freebie(item_name='Item 1', value=10, dev=dev1, company=company1)
+freebie2 = Freebie(item_name='Item 2', value=20, dev=dev2, company=company1)
+freebie3 = Freebie(item_name='Item 3', value=15, dev=dev3, company=company2)
+
+session.add_all([dev1, dev2, dev3, company1, company2, company3, freebie1, freebie2, freebie3])
+
+# Set an ipdb breakpoint to inspect the state
+import ipdb; ipdb.set_trace()
+
+session.commit()
+session.close()
